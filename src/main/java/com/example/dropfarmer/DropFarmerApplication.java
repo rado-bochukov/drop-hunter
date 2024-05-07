5dd2c848c820742d73815d48201a4d1b618dfc33
Radostin Bochukov <radostinbochukov@gmail.com>
2024-05-07 11:44:50 +0300

    initial commit

---
+++ src/main/java/com/example/dropfarmer/DropFarmerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dropfarmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropFarmerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropFarmerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dropfarmer/DropfarmerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dropfarmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropfarmerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
